800a2343858e4080a43bc3d2a65d031a1fb6e484
Dennis Padiernos <depadiernos@gmail.com>
2020-01-07 20:14:25 -0600

    implement  scheme-model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,27 +5,46 @@ const find = () => {
 }
 
 const findById = (id) => {
-  return db("schemes").where("id", id)
+  return db("schemes").where({ id })
 }
 
-const findSteps = () => {}
+const findSteps = (id) => {
+  return db("steps as t")
+    .join("schemes as s")
+    .on("t.scheme_id", "s.id")
+    .where("t.scheme_id", id)
+    .select("t.id", "s.scheme_name", "t.step_number", "t.instructions")
+    .orderBy("t.step_number", "asc")
+}
 
-const add = () => {}
+const add = async (scheme) => {
+  const [id] = await db("schemes").insert(scheme)
+  return findById(id)
+}
 
-const addStep = () => {}
+const addStep = (step, scheme_id) => {
+  
+}
 
-const update = () => {}
+const update = async (newScheme, id) => {
+  await db("schemes")
+    .where({ id })
+    .update(newScheme)
+  return findById(id)
+}
 
-const remove = () => {}
+const remove = (id) => {
+  return db("schemes")
+    .where({ id })
+    .del()
+}
 
 module.exports = {
   find,
   findById,
   findSteps,
   add,
-  findById,
   addStep,
-  findById,
   update,
   remove
 }
